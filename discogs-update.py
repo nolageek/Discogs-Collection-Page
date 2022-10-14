bc3a5711959c2193daf97017a234dff1e35fc3c6
nolageek <nolageek@gmail.com>
2022-10-14 13:21:37 -0400

    Update discogs-update.py
    Added comments, cleaned up some code, etc...

--- discogs-update.py
+++ discogs-update.py
@@ -1,16 +1,11 @@
-#download most recent single release
-#if added date is newer than current files:
-#    update all data files
-#    download all new images
-# https://www.geeksforgeeks.org/command-line-arguments-in-python/
+# Notes for python 3.x:
+#
+# Change all occurances of urllib to urllib.request, including the following
+# import statement as well as all instances of urllib.urlretrieve to
+# urllib.request.urlretrieve
 
-# install validators module
-# pip install validators
+import getopt, sys, json, urllib, os.path
 
-import getopt, sys, json, urllib.request, os.path
-
-# Setting up default values for arguments
-# 
 # You can either hardcode your Discogs token and username here
 # or you can pass them via the --token and/or --username arguments
 DISCOGS_TOKEN = ""
@@ -18,7 +13,10 @@ DISCOGS_USERNAME = ""
 # As of this release set PER to a value larger than your total collection.
 # At one point I may add the ability to create paginated files.
 PER = "500"
-
+#
+# Setting up default values for variables
+# No need to change anything below.
+TYPE = ""
 DISCOGS_API_URL = "https://api.discogs.com"
 DATA_DIR = "./json/"
 ALL_RELEASES_DATA_FILE = "0-added-desc.json"
@@ -42,7 +40,6 @@ try:
      
     # checking each argument
     for currentArgument, currentValue in arguments:
-        
  
         if currentArgument in ("-h", "--help"):
             print ("Displaying Help")
@@ -62,7 +59,8 @@ try:
 except getopt.error as err:
     # output error, and return with an error code
     print (str(err))
-   
+
+# Main loop that takes your option and decides what it's going to do next.
 def main():
 	if TYPE == "updateall":
 			download_folder_list()
@@ -81,31 +79,45 @@ def main():
 	
 	else:
 			print("Invalid action type")
-		
+
+# This downloads the folder data for the collection of the username specified in json format.
+# Each account has a folder id of '0' that contains ALL of their items, and a '1' folder that
+# contains all items that are not in any other folders (uncategorized.) All other folders
+# have a unique numeric ID.
 def download_folder_list():
 	if DISCOGS_TOKEN == "" or DISCOGS_USERNAME == "":
 		print("Missing Discogs Token or Username")
 		exit()
 	FOLDERURL = DISCOGS_API_URL + "/users/" + DISCOGS_USERNAME + "/collection/folders?token=" + DISCOGS_TOKEN
 	print("Downloading Folder List To " + DATA_DIR + FOLDERS_DATA_FILE)
-	urllib.request.urlretrieve(FOLDERURL, DATA_DIR + FOLDERS_DATA_FILE)
-	
+	urllib.urlretrieve(FOLDERURL, DATA_DIR + FOLDERS_DATA_FILE)
+
+# This next function parses the above folder data and loops through each folder and downloads json files
+# containing the collection sorted by both date-added and by artist (each in ascending and descending order.)
+# I was going to add other sorting methods (title for example) but it didn't seem that useful and I wanted
+# to cut down on the number of API calls. Each file has a name in the following format:
+# <ID>-<sort>-<order>.json
+# For example the list(s) of all items in the collection would be 0-added-asc.json, and 0-added-desc.json
 def download_folder_data():
 	with open(DATA_DIR + FOLDERS_DATA_FILE) as folderdata:
 		folders = json.load(folderdata)
 	for folder in folders['folders']:
 		print("Downloading Data For Folder " + folder['name'] + " (ID: " + str(folder['id']) + ", Count: " + str(folder['count']) + ")")
-		urllib.request.urlretrieve(DISCOGS_API_URL + "/users/" + DISCOGS_USERNAME + "/collection/folders/" + str(folder['id']) + "/releases?sort=added&sort_order=asc&per_page=500&token=" + DISCOGS_TOKEN,DATA_DIR + str(folder['id']) + " -added-asc.json")
-		urllib.request.urlretrieve(DISCOGS_API_URL + "/users/" + DISCOGS_USERNAME + "/collection/folders/" + str(folder['id']) + "/releases?sort=added&sort_order=desc&per_page=500&token=" + DISCOGS_TOKEN,DATA_DIR + str(folder['id']) + "-added-desc.json")
-		urllib.request.urlretrieve(DISCOGS_API_URL + "/users/" + DISCOGS_USERNAME + "/collection/folders/" + str(folder['id']) + "/releases?sort=artist&sort_order=asc&per_page=500&token=" + DISCOGS_TOKEN,DATA_DIR + str(folder['id']) + "-artist-asc.json")
-		urllib.request.urlretrieve(DISCOGS_API_URL + "/users/" + DISCOGS_USERNAME + "/collection/folders/" + str(folder['id']) + "/releases?sort=artist&sort_order=desc&per_page=500&token=" + DISCOGS_TOKEN,DATA_DIR + str(folder['id']) + "-artist-desc.json")
+		urllib.urlretrieve(DISCOGS_API_URL + "/users/" + DISCOGS_USERNAME + "/collection/folders/" + str(folder['id']) + "/releases?sort=added&sort_order=asc&per_page=" + PER + "&token=" + DISCOGS_TOKEN,DATA_DIR + str(folder['id']) + " -added-asc.json")
+		urllib.urlretrieve(DISCOGS_API_URL + "/users/" + DISCOGS_USERNAME + "/collection/folders/" + str(folder['id']) + "/releases?sort=added&sort_order=desc&per_page=" + PER + "&token=" + DISCOGS_TOKEN,DATA_DIR + str(folder['id']) + "-added-desc.json")
+		urllib.urlretrieve(DISCOGS_API_URL + "/users/" + DISCOGS_USERNAME + "/collection/folders/" + str(folder['id']) + "/releases?sort=artist&sort_order=asc&per_page=" + PER + "&token=" + DISCOGS_TOKEN,DATA_DIR + str(folder['id']) + "-artist-asc.json")
+		urllib.urlretrieve(DISCOGS_API_URL + "/users/" + DISCOGS_USERNAME + "/collection/folders/" + str(folder['id']) + "/releases?sort=artist&sort_order=desc&per_page=" + PER + "&token=" + DISCOGS_TOKEN,DATA_DIR + str(folder['id']) + "-artist-desc.json")
 
+# This helper function downloads an image when given the url to the image and the release ID (to be used as the file basename.
+# The "name" that can be passed to the function is merely for display while the scripts runs.
+# Image files are located in the ./img directory and are named after the release ID and use the .jpeg extension. ie: ./image/<ID>.jpeg
 def download_image(url,id, name):
 	if name == "":
 		name = "No name Given"
 	print("Downloading image for " + name)
-	urllib.request.urlretrieve(url, IMG_DIR + str(id) + ".jpeg")
-	
+	urllib.urlretrieve(url, IMG_DIR + str(id) + ".jpeg")
+
+# The following function parses the list of all items and if an image file is not found it will be downloaded using download_image().
 def update_images():
 	with open(DATA_DIR + ALL_RELEASES_DATA_FILE) as releasedata:
 		releases = json.load(releasedata)
@@ -115,4 +127,5 @@ def update_images():
 		else:
 			download_image(images['basic_information']['cover_image'],str(images['basic_information']['id']),images['basic_information']['title'])
 
+# Lets run the main() program!
 main()
