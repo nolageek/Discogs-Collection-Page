91902808ffe7da548193db2d21d709471e6f36b3
nolageek <nolageek@gmail.com>
2022-10-20 18:51:51 -0400

    Lotsa(tm) Changes
    This index.php will now check to see if the image directory exists and if the image has not been downloaded - if both are true the images will be downloaded to the image directory. If the image does not exist the image is hotlinked from the discogs server. I will soon add a note saying that the image is being hotlinked to discourage this practice.
    
    I will soon change the included python script to just download all cover images in the collection and get ride of the options to locally store the data files as I dont think its needed anymore.
    
    I've cleaned up some of the code and  added some more icons to make it more visually interesting.

--- index.php
+++ index.php
@@ -1,6 +1,66 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
 
+$DISCOGS_API_URL="https://api.discogs.com";
+$DISCOGS_USERNAME="";
+$DISCOGS_TOKEN="";
+
+$folder_id = "0";
+$sort_by = "added";
+$order = "desc";
+$artist = "all";
+$page = "1";
+$per = "50";
+
+
+// Folder
+
+if(isset($_GET['folder']))
+$folder_id = $_GET['folder'];
+
+if(isset($_GET['sort_by']))
+$sort_by = $_GET['sort_by'];
+
+if(isset($_GET['pagenum']))
+$page_num = $_GET['page_num'];
+
+if(isset($_GET['order'])) 
+$order = $_GET['order'];
+
+if(isset($_GET['page'])) 
+$page = $_GET['page'];
+
+if(isset($_GET['artist']))
+$artist = $_GET['artist'];
+
+if(isset($_GET['per']))
+$per = $_GET['per'];
+
+$options  = array('http' => array('user_agent' => 'DiscogsCollectionPage'));
+$context  = stream_context_create($options);
+$folderjson = $DISCOGS_API_URL . "/users/" . $DISCOGS_USERNAME . "/collection/folders?token=" .$DISCOGS_TOKEN;
+$folderdata = file_get_contents($folderjson, false, $context); // put the contents of the file into a variable
+$folders = json_decode($folderdata,true); // decode the JSON feed
+
+$pagejson = $DISCOGS_API_URL. "/users/" . $DISCOGS_USERNAME . "/collection/folders/" . $folder_id . "/releases?sort=" . $sort_by . "&sort_order=" . $order . "&page=" . $page . "&per_page=" . $per . "&token=" . $DISCOGS_TOKEN;
+$pagedata = file_get_contents($pagejson, false, $context); // put the contents of the file into a variable
+$collection = json_decode($pagedata,true); // decode the JSON feed
+
+// Figure out name, ID and number of items of current folder.
+foreach ($folders['folders'] as $folder) { 
+if ($folder['id'] == $folder_id) {
+$currentfoldername = $folder['name'];
+$currentfoldercount = $folder['count'];
+}
+}
+?>
+    <!DOCTYPE html>
 <html>
 <head>
+
+    <title>Discogs Collection Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=.8">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
 <script src="https://kit.fontawesome.com/7e1a0bb728.js" crossorigin="anonymous"></script>
@@ -32,86 +92,37 @@ body {
 </head>
 
 <body>
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-
-$folder_id = "0";
-$sort_by = "added";
-$order = "desc";
-$artist = "all";
-$page = "1";
-$per = "50";
-
-// Folder
-
-if(isset($_GET['folder']))
-$folder_id = $_GET['folder'];
-
-if(isset($_GET['sort_by']))
-$sort_by = $_GET['sort_by'];
-
-if(isset($_GET['pagenum']))
-$page_num = $_GET['page_num'];
-
-if(isset($_GET['order'])) 
-$order = $_GET['order'];
-
-if(isset($_GET['page'])) 
-$page = $_GET['page'];
-
-if(isset($_GET['artist']))
-$artist = $_GET['artist'];
-
-if(isset($_GET['per']))
-$per = $_GET['per'];
 
 
 
-$DISCOGS_API_URL="https://api.discogs.com";
-$DISCOGS_USERNAME="";
-$DISCOGS_TOKEN="";
+<div class="container-fluid"> <!-- Outer Container -->
 
-$options  = array('http' => array('user_agent' => 'DiscogsCollectionPage'));
-$context  = stream_context_create($options);
-$folderjson = $DISCOGS_API_URL . "/users/" . $DISCOGS_USERNAME . "/collection/folders?token=" .$DISCOGS_TOKEN;
-
-//$folderjson = './json/folders.json'; // path to your FOLDER JSON file
-$folderdata = file_get_contents($folderjson, false, $context); // put the contents of the file into a variable
-$folders = json_decode($folderdata,true); // decode the JSON feed
-
-$pagejson = $DISCOGS_API_URL. "/users/" . $DISCOGS_USERNAME . "/collection/folders/" . $folder_id . "/releases?sort=" . $sort_by . "&sort_order=" . $order . "&page=" . $page . "&per_page=" . $per . "&token=" . $DISCOGS_TOKEN;
-//$pagejson = 'json/' . $folder_id . "-" . $sort_by . "-" . $order . '.json'; // path to your JSON file
-$pagedata = file_get_contents($pagejson, false, $context); // put the contents of the file into a variable
-$collection = json_decode($pagedata,true); // decode the JSON feed
-
-
-// Figure out name, ID and number of items of current folder.
-foreach ($folders['folders'] as $folder) { 
-if ($folder['id'] == $folder_id) {
-$currentfoldername = $folder['name'];
-$currentfoldercount = $folder['count'];
-}
-}
-?>
-
-
-<div class="container-fluid">
-	    <!-- For demo purpose -->
-
-    <div class="row py-1">
-      <div class="col-lg-12 mx-auto">
-        <div class="text-white p-5 shadow-sm rounded banner">
-          <h1 class="display-4">Discogs Collection Page</h1>
+ <!-- Banner header -->
+    <div class="row">
+      <div class="col-12 mx-auto my-1">
+        <div class="text-white p-3 shadow-sm rounded banner">
+          <h2 class="display-6">Discogs Collection Page for <?php echo $DISCOGS_USERNAME ?></h2>
           <p class="lead">[<?php echo $currentfoldername;?>] (<?php echo $currentfoldercount;?> items) Sorted by: <?php echo $sort_by ?>, <?php echo $order ?>ending</p>
         </div>
       </div>
     </div>
-    <!-- End -->
-	
-  <div class="px-lg-5">
-    <div class="py-5 text-center">
+    <!-- Banner header End -->
 
+
+<!-- Pagination / Nav / Filter Bar-->
+<div class="btn-toolbar d-flex justify-content-center p-3" role="toolbar" aria-label="Toolbar with button groups">      
+    <div class="btn-group btn-group-sm mr-2 p-1" role="group" aria-label="Pagination">
+        <a class="btn btn-primary text-uppercase <?php if($page == 1) echo "disabled"; ?>" href="/?folder=<?php echo $folder_id; ?>&sort_by=<?php echo $sort_by; ?>&order=<?php echo $order; ?>&per=<?php echo $per; ?>&page=<?php if($page != 1) echo (intval($page) - 1); ?>" tabindex="-1">&#12298;</a>
+<?php 	$x = 1;
+		$pages = $collection['pagination']['pages'];
+		while($x <= $pages) {?>
+		<a class="btn btn-primary text-uppercase <?php if($page == $x) echo "active disabled"; ?>" href="/?folder=<?php echo $folder_id; ?>&sort_by=<?php echo $sort_by; ?>&order=<?php echo $order; ?>&per=<?php echo $per; ?>&page=<?php echo $x;?>"><?php echo $x;?></a>
+		<?php $x++; } 
+?>
+		<a class="btn btn-primary text-uppercase <?php if($page == $collection['pagination']['pages']) echo "disabled"; ?>" href="/?folder=<?php echo $folder_id; ?>&sort_by=<?php echo $sort_by; ?>&order=<?php echo $order; ?>&per=<?php echo $per; ?>&page=<?php if($page != $pages) echo (intval($page) + 1); ?>" tabindex="-1">&#12299;</a>
+	</div>
+	    <div class="btn-group btn-group-sm mr-2 p-1" role="group" aria-label="Folder Navigation">
+		
 <?php
 
 foreach ($folders['folders'] as $folder) { 
@@ -121,46 +132,27 @@ $foldercount = $folder['count'];
 
 if ($foldercount > 0) {
 ?>
-    <a href="/?folder=<?php echo $folderid; ?>&sort_by=<?php echo $sort_by; ?>&order=<?php echo $order; ?>&per=<?php echo $per; ?>&page=1" class="btn btn-primary px-2 py-1 text-uppercase<?php if ($folder_id == $folderid) echo " disabled"; ?>"><?php echo $foldername; ?> (<?php echo $foldercount; ?>)</a>
+    <a href="/?folder=<?php echo $folderid; ?>&sort_by=<?php echo $sort_by; ?>&order=<?php echo $order; ?>&per=<?php echo $per; ?>&page=1" class="btn btn-primary text-uppercase<?php if ($folder_id == $folderid) echo " disabled"; ?>"><?php echo $foldername; ?> (<?php echo $foldercount; ?>)</a>
 <?php } } ?>
-
-    <?php if ($sort_by == "artist") { ?>
-    <a href="/?folder=<?php echo $folder_id; ?>&sort_by=added&order=<?php echo $order; ?>&per=<?php echo $per; ?>&page=<?php echo $page; ?>" class="btn btn-info px-2 py-1 text-uppercase">Added</a>
-    <?php } else { ?>
-    <a href="/?folder=<?php echo $folder_id; ?>&sort_by=artist&order=<?php echo $order; ?>&page=<?php echo $page; ?>" class="btn btn-info px-2 py-1 text-uppercase">Artist</a> 
-<?php } ?>
-    <a href="/?folder=<?php echo $folder_id; ?>&sort_by=<?php echo $sort_by; ?>&order=asc&per=<?php echo $per; ?>" class="btn btn-secondary px-2 py-1 text-uppercase<?php if ($order == "asc") echo " disabled"; ?>">ASC</a>
-     <a href="/?folder=<?php echo $folder_id; ?>&sort_by=<?php echo $sort_by; ?>&order=desc&per=<?php echo $per; ?>&page=<?php echo $page; ?>" class="btn btn-secondary px-2 py-1 text-uppercase<?php if ($order == "desc") echo " disabled"; ?>">DESC</a> 
-     
-
- <!--    <a href="/?folder=<?php echo $folder_id; ?>-title" class="btn btn-dark px-5 py-3 text-uppercase">Title</a> 
-     <a href="/?folder=<?php echo $folder_id; ?>-year" class="btn btn-dark px-5 py-3 text-uppercase">Year (asc)</a>
-     <a href="/?folder=vinyl-year&sort=desc" class="btn btn-dark px-5 py-3 text-uppercase">Year (desc)</a>-->
-
     </div>
-	
-  </div>
+    <div class="btn-group btn-group-sm mr-2 p-1" role="group" aria-label="Sort by Artist or Date Added">
+    <?php if ($sort_by == "artist") { ?>
+    <a href="/?folder=<?php echo $folder_id; ?>&sort_by=added&order=<?php echo $order; ?>&per=<?php echo $per; ?>&page=<?php echo $page; ?>" class="btn btn-info text-uppercase">Added</A>
+    <?php } else { ?>
+    <a href="/?folder=<?php echo $folder_id; ?>&sort_by=artist&order=<?php echo $order; ?>&page=<?php echo $page; ?>" class="btn btn-info text-uppercase">Artist</a> 
+<?php } ?>
+    </div>
+    <div class="btn-group btn-group-sm  p-1" role="group" aria-label="Ascending or Descending">
+    <a href="/?folder=<?php echo $folder_id; ?>&sort_by=<?php echo $sort_by; ?>&order=asc&per=<?php echo $per; ?>" class="btn btn-secondary text-uppercase<?php if ($order == "asc") echo " disabled"; ?>">ASC</a>
+     <a href="/?folder=<?php echo $folder_id; ?>&sort_by=<?php echo $sort_by; ?>&order=desc&per=<?php echo $per; ?>&page=<?php echo $page; ?>" class="btn btn-secondary text-uppercase<?php if ($order == "desc") echo " disabled"; ?>">DESC</a> 
+     
+    </div>
+    </div> <!-- Pagination / Nav / Filter Bar-->
 
-<nav aria-label="...">
-  <ul class="pagination">
-    <li class="page-item <?php if($page == 1) echo "disabled"; ?>">
-	      <a class="page-link" href="/?folder=<?php echo $folder_id; ?>&sort_by=<?php echo $sort_by; ?>&order=<?php echo $order; ?>&per=<?php echo $per; ?>&page=<?php if($page != 1) echo (intval($page) - 1); ?>" tabindex="-1">Previous</a>
+  
 
-    </li>
-	
-<?php 	$x = 1;
-		$pages = $collection['pagination']['pages'];
-		while($x <= $pages) {?>
-		
-    <li class="page-item <?php if($page == $x) echo "active"; ?>"><a class="page-link" href="/?folder=<?php echo $folder_id; ?>&sort_by=<?php echo $sort_by; ?>&order=<?php echo $order; ?>&per=<?php echo $per; ?>&page=<?php echo $x;?>"><?php echo $x;?></a></li>
-		<?php $x++; } ?>
-		
-    <li class="page-item <?php if($page == $collection['pagination']['pages']) echo "disabled"; ?>">
-      <a class="page-link" href="/?folder=<?php echo $folder_id; ?>&sort_by=<?php echo $sort_by; ?>&order=<?php echo $order; ?>&per=<?php echo $per; ?>&page=<?php if($page != $pages) echo (intval($page) + 1); ?>" tabindex="-1">Next</a>
-    </li>
-  </ul>
-</nav>
 
+<!-- Gallery of Releases -->
   <div class="row">
   
 
@@ -179,42 +171,55 @@ if ($release['basic_information']['formats'][0]['descriptions'])
   $formatdesc = implode(", ", $release['basic_information']['formats'][0]['descriptions']);
 $genres = implode(", ", $release['basic_information']['genres']);
 $styles = implode(", ", $release['basic_information']['styles']);
-$notes = implode(", ", $release['basic_information']['notes']);
-
-
+$notes = "Notes not working yet.";
+$imageupdatedtext = "";
+$imagefile = "./img/" . $release['basic_information']['id'] . ".jpeg"; 
+              if (!file_exists($imagefile) && is_dir( "./img/" ) ) {
+                $imageupdatedtext = "Missing file has been downloaded from Discogs server.";  
+                $imagename = file_get_contents($release['basic_information']['cover_image']);
+                file_put_contents($imagefile, $imagename);
+              }
 ?>
 
 
 <!-- Gallery item -->
-<div class="col-xl-3 col-lg-4 col-md-6 mb-4">
-        <?php $imagefile = "img/" . $release['basic_information']['id'] . ".jpeg"; 
-              if (!file_exists($imagefile))
-                $imagefile = "no-album-art.png";
+<div class="col-xl-3 col-lg-6 col-md-6 mb-4">
+    
+    
+        <?php 
               ?>
 
         <div class="bg-white rounded shadow-sm"><a data-toggle="collapse" href=".multi-collapse<?php echo $release['basic_information']['id']; ?>" role="button" aria-expanded="false" aria-controls="<?php echo $release['basic_information']['id']; ?>A <?php echo $release['basic_information']['id']; ?>B $release['basic_information']['id']; ?>C" ><img src="<?php echo $imagefile; ?>" alt="" class="img-fluid card-img-top"></a>
+        
+                        <?php echo $imageupdatedtext; ?>
+                
           <div class="p-4">
 
             <table class="table table-striped">
             <tbody>
     <tr>
-      <th scope="row">Title</th>
+      <th scope="row"><i class="fa-fw fa-solid fa-quote-right"></i></th>
+      <td>Title</td>
       <td><?php echo $title; ?></td>
     </tr>                
     <tr>
-      <th scope="row">Artist</th>
+      <th scope="row"><i class="fa-fw fa-solid fa-people-group"></i></th>
+      <td>Artist</td>
       <td><?php echo $artists; ?></td>
     </tr>
     <tr class="collapse multi-collapse<?php echo $id; ?>" id='<?php echo $id; ?>A'>
-      <th scope="row">Released</th>
+      <th scope="row"><i class="fa-fw fa-solid fa-calendar-days"></i></th>
+      <td>Released</td>
       <td><?php echo $year; ?></td>
     </tr>
     <tr class="collapse multi-collapse<?php echo $id; ?>" id='<?php echo $id; ?>A'>
-      <th scope="row">Label</th>
+      <th scope="row"><i class="fa-fw fa-solid fa-building"></i></th>
+      <td>Label</td>
       <td><?php  if( $labelname ) echo $labelname; ?></td>
     </tr>
-    <tr class="collapse multi-collapse<?php echo id; ?>" id='<?php echo $id; ?>B'>
-      <th scope="row">Format</th>
+    <tr class="collapse multi-collapse<?php echo $id; ?>" id='<?php echo $id; ?>B'>
+      <th scope="row"><i class="fa-fw fa-solid fa-compact-disc"></i></th>
+      <td>Format</td>
        <td>
         <?php  if( $formatname ) echo $formatname;
                if( $formattext ) echo ", " . $formattext;  
@@ -223,14 +228,16 @@ $notes = implode(", ", $release['basic_information']['notes']);
         </td>
     </tr>
     <tr class="collapse multi-collapse<?php echo $id; ?>" id='<?php echo $id; ?>C'>
-      <th scope="row">Genres</th>
+      <th scope="row"><i class="fa-fw fa-solid fa-bars-staggered"></i></th>
+      <td>Genres</td>
        <td>
          <?php   echo $genres; 
                  if( $styles ) echo ", "  . $styles; ?>
       </td>
       </tr>
     <tr class="collapse multi-collapse<?php echo $id; ?>" id='<?php echo $id; ?>B'>
-      <th scope="row">Notes</th>
+      <th scope="row"><i class="fa-fw fa-solid fa-file-pen"></i></th>
+      <td>Notes</td>
        <td>
        <?php if($notes) echo $notes;
       ?>
@@ -239,26 +246,26 @@ $notes = implode(", ", $release['basic_information']['notes']);
 </tbody>
 </table>
  
-            <div class="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4">
+            <div class="d-flex align-items-center justify-content-between bg-light px-3 py-2 mt-4">
               <p class="small mb-0">+<?php $adddate = $release['date_added']; echo date('m/d/y', strtotime(substr($adddate,0,10))) ?></p>
               <?php $todaydate = date("Y-m-d");
                if(strtotime($adddate) > strtotime('-10 days')) {
                 ?>
               <div class="badge badge-danger px-3 rounded-pill font-weight-normal">New</div>
               <?php  } ?>
-              <div><a class="btn btn-primary btn-sm" href="https://www.discogs.com/release/<?php echo $id ?>">Discogs <i class="fa-solid fa-arrow-up-right-from-square"></i></a></div>
+              <div><a class="btn btn-secondary btn-sm" href="https://www.discogs.com/release/<?php echo $id ?>">Discogs <i class="fa-solid fa-arrow-up-right-from-square"></i></a></div>
             </div>
           </div>
         </div>
       </div>
-	  <!-- End gallery Item
+	  <!-- End gallery Item -->
 
 <?php } ?>
 
-</div>
+</div> <!-- Gallery of Releases End -->
     <div class="py-5 text-center"><a href="#" class="btn btn-dark px-5 py-3 text-uppercase">BACK TO TOP</a></div>
-  </div>
-</div>
+  </div> <!-- Outer Container End -->
+
 
 
 </body>
